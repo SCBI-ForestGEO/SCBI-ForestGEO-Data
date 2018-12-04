caf71c4a922ed17b8b55df4935ea262437f683c4
mcgregorian1 <mcgregori@si.edu>
2018-12-04 10:53:56 -0500

    Created early analyses for paper
    Created dbh by canopy positions by sp

---
+++ tree_dimensions/tree_crowns/dbh_canopyposition_by_sp.R
@@ -0,0 +1,43 @@
+# analysis for Ian paper
+
+setwd("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_dimensions/tree_crowns")
+
+dendrofull <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_dimensions/tree_crowns/dendro_cored_full.csv")
+
+#dataframe subsets ####
+## separate chronologies by canopy position
+
+highcan <- subset(dendrofull, (crown.position == "D" | crown.position == "C") & status == "alive")
+lowcan <- subset(dendrofull, (crown.position == "I" | crown.position == "S") & status == "alive")
+
+## separate trees by dbh (above and below 35cm)
+highdbh <- subset(dendrofull, dbh2018>350 & status == "alive")
+lowdbh <- subset(dendrofull, dbh2018<=350 & status == "alive")
+
+#graphs #####
+library(ggplot2)
+
+pdf(file="DBH_CanopyPosition_by_Sp.pdf", width=10)
+
+## graph DBH abundance by canopy position
+alive <- subset(dendrofull, status == "alive")
+ggplot(data = alive) +
+  aes(x = dbh2018, fill = crown.position) +
+  geom_histogram(bins = 50) +
+  scale_fill_brewer(palette = "Paired") +
+  scale_x_continuous(breaks=c(0,350,1500)) +
+  labs(title = "DBH by Canopy Position",
+       x = "dbh2018",
+       y = "Count") +
+  theme_minimal()
+
+## graph DBH by canopy position and sp
+ggplot(data = alive) +
+  aes(x = sp,fill = crown.position, weight = dbh2018/100) +
+  geom_bar() +
+  scale_y_continuous(breaks=c(0,350,1500)) +
+  labs(title = "DBH and Canopy Position by Sp",
+       x = "sp",
+       y = "dbh (mm)") +
+  theme_minimal()
+dev.off()
