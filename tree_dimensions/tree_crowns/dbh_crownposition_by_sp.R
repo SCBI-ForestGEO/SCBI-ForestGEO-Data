caa414510bfecf994e2766ef432551f0de4247b9
mcgregorian1 <mcgregori@si.edu>
2018-12-05 14:44:55 -0500

    Updated numbers in script

--- tree_dimensions/tree_crowns/dbh_crownposition_by_sp.R
+++ tree_dimensions/tree_crowns/dbh_crownposition_by_sp.R
@@ -1,12 +1,12 @@
 # analysis for Ian paper
 
-## This analysis uses a very specific subset of cored trees, starting only with the 14 species used in Ryan Helcoski's paper (which is 727 individuals (726 tags, 1 multistem)). 
+## This analysis uses a very specific subset of cored trees, starting only with the 14 species used in Ryan Helcoski's paper (which is 726 individuals (725 tags, 1 multistem)). 
 
 ## Of these, those with a dbh of 0 in 2018 were assigned the dbh in 2013, and assumed to have a crown position of "C" (if dbh>350mm) or "I" (if dbh<=350 but dbh>0), if there wasn't a crown position measured.
 
 ## The remaining individuals with dbh=0 (meaning they died 2013 and prior) were filtered out. Individuals with a crown position of NA were also filtered out (bc some of them are labeled as fallen, or as DN this year). By this point all of AJ's (SMSC student) measurements of "CF" (couldn't find) from fall 2018 have been converted.
 
-## After all this filtering, the graphs finally show the remaining 666 individuals (665 tags plus 1 multistem).
+## After all this filtering, the graphs finally show the remaining 664 individuals (663 tags plus 1 multistem).
 
 #which cores used in final chronologies ####
 #these cores used in Ryan's paper
@@ -92,4 +92,47 @@ ggplot(data = dendro2018) +
        y = "dbh2018 (mm)") +
   theme_minimal()
 dev.off()
+
+
+
+finalsp <- levels(factor(dendro2018$sp))
+
+
+totals$crown.S <- length(which(litu$crown.position=="S"))
+
+library(stringr)
+
+totals <- data.frame(finalsp)
+
+# for intraannual
+for (j in seq(along=finalsp)){
+  spname = finalsp[[j]]
+  totals$crown.S <- ifelse(sum(dendro2018$crown.position == "S")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(data.table)
+testr <- dendro2018[ ,c("sp", "crown.position")]
+testr$ <
+
+library(dplyr)
+test$C <- testr %>% 
+  add_count(sp, crown.position=="c")
+test <- test[!(duplicated(test)),]
+
+
+
+
 
