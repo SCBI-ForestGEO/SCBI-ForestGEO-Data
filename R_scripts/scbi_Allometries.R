e2684768ec9dfec96c632d57dc47fa13067ffb19
Erika Gonzalez-Akre <gonzalezeb@si.edu>
2019-04-04 17:06:16 -0400

    Update scbi_Allometries.R

--- R_scripts/scbi_Allometries.R
+++ R_scripts/scbi_Allometries.R
@@ -1,5 +1,10 @@
-#scbi biomass Code
-##########################################################################
+######################################################
+# Purpose: Estimate AGB for stems at SCBI. 
+# Developped by: Valentine Herrmann - HerrmannV@si.edu
+# Date: ??
+#######################################################
+
+
 ## x is a table with:
 ## - a column called 'dbh' which has the dbh of all the stems in mm
 ## - a column called 'SPPCODE' with species 4-letter species code 
