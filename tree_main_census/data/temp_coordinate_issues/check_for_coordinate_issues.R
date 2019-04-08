49714a85b69bf2e6b876515b7b35b0462e2f96e6
mcgregorian1 <mcgregori@si.edu>
2019-04-08 15:23:05 -0400

    Update check_for_coordinate_issues.R

--- tree_main_census/data/temp_coordinate_issues/check_for_coordinate_issues.R
+++ tree_main_census/data/temp_coordinate_issues/check_for_coordinate_issues.R
@@ -17,6 +17,8 @@ check_2008 <- scbi.stem1[scbi.stem1$lx <0 | scbi.stem1$lx>20 | scbi.stem1$ly<0 |
 read.csv("scbi.stem2.csv")
 scbi.stem2 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.stem2.csv")
 
+scbi.stem2$tags_comb <- paste0(scbi.stem2$tag, "_", scbi.stem2$StemTag)
+
 scbi.stem2$lx <- scbi.stem2$gx - 20*((scbi.stem2$quadrat %/% 100) - 1)
 scbi.stem2$ly <- scbi.stem2$gy - 20*((scbi.stem2$quadrat %% 100) - 1)
 
@@ -29,10 +31,10 @@ check_2013 <- scbi.stem2[scbi.stem2$lx <0 | scbi.stem2$lx>20 | scbi.stem2$ly<0 |
 scbi.stem3 <- read.csv("scbi.stem3_TEMPORARY.csv") #from github
 scbi.stem3 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_main_census/data/scbi.stem3_TEMPORARY.csv")
 
-scbi.stem3$Tree_ID_Num <- gsub("_[[:digit:]]", "", scbi.stem3$Tree_ID_Num)
+scbi.stem3$tag <- gsub("_[[:digit:]]", "", scbi.stem3$Tree_ID_Num)
 
 library(data.table)
-setnames(scbi.stem3, old=c("Tree_ID_Num", "SiteID", "DBHcm", "SPPCODE"), new=c("tag", "quadrat", "dbh", "sp"))
+setnames(scbi.stem3, old=c("Tree_ID_Num", "SiteID", "DBHcm", "SPPCODE"), new=c("tags_comb", "quadrat", "dbh", "sp"))
 
 scbi.stem3$lx <- scbi.stem3$gx - 20*((scbi.stem3$quadrat %/% 100) - 1)
 scbi.stem3$ly <- scbi.stem3$gy - 20*((scbi.stem3$quadrat %% 100) - 1)
@@ -47,12 +49,16 @@ check_2008$tag %in% check_2013$tag
 check_2013$tag %in% check_2018$tag
 
 #because 2008 and 2013 are identical, I'm only rbinding 2013 and 2018
-check_2013$year <- ifelse(check_2013$tag %in% check_2018$tag, c("2008, 2013, 2018"), c("2008, 2013"))
-
 need <- setdiff(colnames(check_2013), colnames(check_2018))
 extra <- setdiff(colnames(check_2018), colnames(check_2013))
 check_2013[, extra] <- NA
 check_2018[, need] <- NA
 combine <- rbind(check_2013, check_2018)
 
-combine <- combine[!duplicated(combine$tag), ]
+#some tag/stemtag combos are duplicated between 2013 and 2018
+combine <- combine[!duplicated(combine$tags_comb), ]
+combine$year <- ifelse(combine$tag %in% check_2018$tag & combine$tag %in% check_2013$tag, 
+                       c("2008, 2013, 2018"), 
+                       ifelse(combine$tag %in% check_2013$tag & !(combine$tag %in% check_2018$tag), c("2008, 2013"), combine$year))
+
+write.csv(combine, "combined_years.csv", row.names=FALSE)
