78aca0545b382a3aaf64c4965695663711630450
mcgregorian1 <mcgregori@si.edu>
2019-04-08 15:00:51 -0400

    Update check_for_coordinate_issues.R

--- tree_main_census/data/temp_coordinate_issues/check_for_coordinate_issues.R
+++ tree_main_census/data/temp_coordinate_issues/check_for_coordinate_issues.R
@@ -17,8 +17,6 @@ check_2008 <- scbi.stem1[scbi.stem1$lx <0 | scbi.stem1$lx>20 | scbi.stem1$ly<0 |
 read.csv("scbi.stem2.csv")
 scbi.stem2 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.stem2.csv")
 
-scbi.stem2$tag <- as.character(scbi.stem2$tag)
-
 scbi.stem2$lx <- scbi.stem2$gx - 20*((scbi.stem2$quadrat %/% 100) - 1)
 scbi.stem2$ly <- scbi.stem2$gy - 20*((scbi.stem2$quadrat %% 100) - 1)
 
@@ -34,7 +32,7 @@ scbi.stem3 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI
 scbi.stem3$Tree_ID_Num <- gsub("_[[:digit:]]", "", scbi.stem3$Tree_ID_Num)
 
 library(data.table)
-setnames(scbi.stem3, old=c("Tree_ID_Num", "SiteID"), new=c("tag", "quadrat"))
+setnames(scbi.stem3, old=c("Tree_ID_Num", "SiteID", "DBHcm", "SPPCODE"), new=c("tag", "quadrat", "dbh", "sp"))
 
 scbi.stem3$lx <- scbi.stem3$gx - 20*((scbi.stem3$quadrat %/% 100) - 1)
 scbi.stem3$ly <- scbi.stem3$gy - 20*((scbi.stem3$quadrat %% 100) - 1)
@@ -43,3 +41,18 @@ scbi.stem3$lx <- round(scbi.stem3$lx, digits=1)
 scbi.stem3$ly <- round(scbi.stem3$ly, digits=1)
 
 check_2018 <- scbi.stem3[scbi.stem3$lx <0 | scbi.stem3$lx>20 | scbi.stem3$ly<0 | scbi.stem3$ly>20, ]
+
+## compare the years ####
+check_2008$tag %in% check_2013$tag
+check_2013$tag %in% check_2018$tag
+
+#because 2008 and 2013 are identical, I'm only rbinding 2013 and 2018
+check_2013$year <- ifelse(check_2013$tag %in% check_2018$tag, c("2008, 2013, 2018"), c("2008, 2013"))
+
+need <- setdiff(colnames(check_2013), colnames(check_2018))
+extra <- setdiff(colnames(check_2018), colnames(check_2013))
+check_2013[, extra] <- NA
+check_2018[, need] <- NA
+combine <- rbind(check_2013, check_2018)
+
+combine <- combine[!duplicated(combine$tag), ]
