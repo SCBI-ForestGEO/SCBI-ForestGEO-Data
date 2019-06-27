8ac508df9f1a9d5b4941185a567a8ec7787ab5c3
Ian McGregor <mcgregori@si.edu>
2019-06-27 11:03:55 -0400

    Consolidated the other distribution map script from the plot book repo

--- R_scripts/species_distribution_maps.R
+++ R_scripts/species_distribution_maps.R
@@ -4,29 +4,34 @@
 # R version 3.5.2 - First created April 2019
 ###############################################
 
-library(fgeo) #sp is loaded with this package
-library(ggplot2)
-library(rgdal)
-library(broom) # for the tidy function
-library(sf) # for mapping
-library(ggthemes) # needed for plot theme
+library(RCurl) #1
+library(rgdal) #2a
+library(fgeo) #2a, package sp is loaded with this package
+library(broom) #2a for the tidy function
+library(ggplot2) #3, 4
+library(sf) #3 for mapping
+library(ggthemes) #3 needed for plot theme
 
-# Script to go from SIGEO quadrat coordinates to SIGEO grid coordinates to NAD83 coordinates #####
-# Following chunk of code is sourced from the original found within the 'spatial_data' folder located in the SCBI-ForestGEO-Data repo on GitHub
 
-# Using 2018 data
-library(RCurl) 
+#1 Convert SIGEO coordinates to decimal degrees and UTM NAD83 coordinates #####
+# This chunk of code is sourced from the original found within the 'spatial_data' folder located in the SCBI-ForestGEO-Data repo on GitHub
+## contributors: Dunbar Carpenter, Jonathan Thompson, Erika Gonzalez-Akre, Ian McGregor
 
-sigeo <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/scbi.stem3_TEMPORARY.csv"))
+##NOTES FOR #1
+###until the 2018 data is public, this raw website will need to be sporadically updated because it is coming from a private repo
+###this means the column names WILL need to be updated eventually
+sigeo <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data_private/master/census%20data/ViewFullTable_crc_master.csv?token=AJNRBEKU5NOO5E7BOP73SHS5DYGNI"), stringsAsFactors = FALSE)
+
+sigeo <- sigeo[sigeo$CensusID == 3, ]
 
 # plot grid coordinates to see if they make sense
-plot(sigeo$gx, sigeo$gy)
+plot(sigeo$PX, sigeo$PY)
 
 ## get local coordinates by calculating based on grid coordinates.
 ## To replicate, you subtract gx by (20*(quadrat divided by 100) minus 1).
 ## For gy, it's the same thing but the remainder of the quadrat divided by 100.
-sigeo$lx <- sigeo$gx - 20*((sigeo$SiteID %/% 100) - 1)
-sigeo$ly <- sigeo$gy - 20*((sigeo$SiteID %% 100) - 1)
+sigeo$lx <- sigeo$PX - 20*((sigeo$QuadratName %/% 100) - 1)
+sigeo$ly <- sigeo$PY - 20*((sigeo$QuadratName %% 100) - 1)
 
 #round local coordinates to nearest tenth
 sigeo$lx <- round(sigeo$lx, digits = 1)
@@ -49,7 +54,7 @@ grid2nad83 <- function(x, y) {
 }
 
 ## add NAD83 coordinate columns to SIGEO data table
-sigeo <- data.frame(sigeo, grid2nad83(sigeo$gx, sigeo$gy))
+sigeo <- data.frame(sigeo, grid2nad83(sigeo$PX, sigeo$PY))
 
 # Add lat lon to the file, first run these 2 lines
 utmcoor <- SpatialPoints(cbind(sigeo$NAD83_X, sigeo$NAD83_Y), proj4string=CRS("+proj=utm +zone=17N"))
@@ -60,9 +65,8 @@ sigeo$lat <- coordinates(longlatcoor)[,2]
 sigeo$lon <- coordinates(longlatcoor)[,1]
 plot(sigeo$lon, sigeo$lat)
 
-# following part of script is to create maps species within the plot ####
-
-## files can be found in the ForestGEO-Data repo on GitHub
+#2 Load data to create maps ####
+##2a. Load in shapefiles ####
 scbi_plot <- readOGR("spatial_data/shapefiles/20m_grid.shp")
 ForestGEO_grid_outline <- readOGR("spatial_data/shapefiles/ForestGEO_grid_outline.shp")
 deer <- readOGR("spatial_data/shapefiles/deer_exclosure_2011.shp")
@@ -70,50 +74,42 @@ roads <- readOGR("spatial_data/shapefiles/SCBI_roads_edits.shp")
 streams <- readOGR("spatial_data/shapefiles/SCBI_streams_edits.shp")
 contour_10m <- readOGR("spatial_data/shapefiles/contour10m_SIGEO_clipped.shp")
 
-
-# convert all shp to dataframe so that it can be used by ggplot ####
-# if tidy isn't working, can also do: xxx_df <- as(xxx, "data.frame")
-
 #Use this option if you want to visualize the plot WITH quadrat/grid lines
-scbi_plot_df <- tidy(scbi_plot)
+# scbi_plot_df <- tidy(scbi_plot)
 
 #Use this option if you want to visualize the plot WITHOUT quadrat/grid lines
-#ForestGEO_grid_outline_df <- tidy(ForestGEO_grid_outline) 
+scbi_plot_df <- tidy(ForestGEO_grid_outline) 
 
+###if tidy isn't working, can also do: xxx_df <- as(xxx, "data.frame")
 deer_df <- tidy(deer)
 roads_df <- tidy(roads)
 streams_df <- tidy(streams)
 contour_10m_df <- tidy(contour_10m)
 
-# x and y give the x/yposition on the plot; sprintf says to add 0 for single digits, the x/y=seq(...,length.out) says fit the label within these parameters, fitting the length of the label evenly.
 
-## this code adds the row and column numbers based on coordinates
-rows <- annotate("text", x = seq(747350, 747365, length.out = 32), y = seq(4309125, 4308505, length.out = 32), label = sprintf("%02d", 32:1), size = 3, color = "black")
-
-cols <- annotate("text", x = seq(747390, 747765, length.out = 20), y = seq(4308495, 4308505, length.out = 20), label = sprintf("%02d", 1:20), size = 2.8, color = "black")
-
-# this will be the foundation of where the data points from each species within each census will go ####
-
-### needed to add contour lines - online research says that function needed to do this is upgraded to current R version yet
+#2b. Get contour labels ####
+##needed to add contour lines - online research says that function needed to do this is upgraded to current R version yet
 # library(directlabels)
 # direct.label.ggplot(ggplot_test, method="bottom.pieces")
 
-# make a for loop for all species
-sigeo$Tree_Status <- as.character(sigeo$Tree_Status)
+elevation_labels <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/spatial_data/elevation/contour10m_SIGEO_coords.csv"), stringsAsFactors = FALSE)
 
-sigeo$Tree_Status[sigeo$Tree_Status %in% c("Dead")] <- "dead"
-sigeo$Tree_Status[sigeo$Tree_Status %in% c("Live", "prior", "lost_stem")] <- "live"
+#3. Loop to create maps for all species ####
+##reminder as from #1 above, these column names and statuses WILL need to be changed when the 2018 data goes public
 
-# for loop for making maps for all species
-for(i in seq(along = unique(sigeo$SPPCODE))){
-  focus_sp <- unique(sigeo$SPPCODE)[[i]]
-  focus_sp_df <- sigeo[sigeo$SPPCODE == focus_sp, ]
-  focus_sp_alive <- subset(focus_sp_df, Tree_Status == "live")
-  focus_sp_dead <- subset(focus_sp_df, Tree_Status == "dead")
+sigeo$Status <- ifelse(grepl("dead", sigeo$Status), "dead", "live")
+sigeo$DBH <- ifelse(grepl("dead", sigeo$Status), 0, sigeo$DBH)
+sigeo$DBH <- as.numeric(sigeo$DBH)
+
+for(i in seq(along = unique(sigeo$Mnemonic))){
+  focus_sp <- unique(sigeo$Mnemonic)[[i]]
+  focus_sp_df <- sigeo[sigeo$Mnemonic == focus_sp, ]
+  focus_sp_alive <- subset(focus_sp_df, Status == "live")
+  focus_sp_dead <- subset(focus_sp_df, Status == "dead")
   
   #ggplot code ####
   ggplot_test <- ggplot() +
-    geom_point(data = focus_sp_alive, aes(x = NAD83_X, y = NAD83_Y, color = DBHcm)) +
+    geom_point(data = focus_sp_alive, aes(x = NAD83_X, y = NAD83_Y, color = DBH)) +
     #color for focus_sp_dead = black by default!! This is not included in legend.
     geom_point(data = focus_sp_dead, aes(x = NAD83_X, y = NAD83_Y)) +
     #geom_path(data = ForestGEO_grid_outline_df, aes(x = long, y = lat, group = group)) +
@@ -138,10 +134,18 @@ for(i in seq(along = unique(sigeo$SPPCODE))){
     theme(panel.grid.major = element_line(colour = 'transparent')) +
     theme(legend.position = "bottom", legend.box = "horizontal") +
     theme(panel.background = element_rect(fill = "gray98")) +
-    ggtitle(paste0(focus_sp, "_2018")) +
-    rows +
-    cols
+    ggtitle(paste0(focus_sp, "_2018"))
   
   #save maps to folder
   ggsave(filename = paste0("spatial_data/maps/species_maps/", focus_sp, ".jpg"), plot = ggplot_test)
 }
+
+#4. Add in row and column numbers to plot graphs ####
+## this code adds the row and column numbers based on coordinates
+rows <- annotate("text", x = seq(747350, 747365, length.out = 32), y = seq(4309125, 4308505, length.out = 32), label = sprintf("%02d", 32:1), size = 3, color = "black")
+
+cols <- annotate("text", x = seq(747390, 747765, length.out = 20), y = seq(4308495, 4308505, length.out = 20), label = sprintf("%02d", 1:20), size = 2.8, color = "black")
+# x and y give the x/yposition on the plot; sprintf says to add 0 for single digits, the x/y=seq(...,length.out) says fit the label within these parameters, fitting the length of the label evenly.
+
+##add this code at end of loop above and before saving
+ggplot_test <- ggplot_test + rows + cols
