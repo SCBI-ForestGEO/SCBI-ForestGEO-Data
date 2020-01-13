3195b363a89bf5ae6c94a3cfc706897c2e5128c9
Erika Gonzalez-Akre <gonzalezeb@si.edu>
2020-01-13 17:19:07 -0500

    Update scbi_Allometries.R

--- R_scripts/scbi_Allometries.R
+++ R_scripts/scbi_Allometries.R
@@ -5,9 +5,10 @@
 #######################################################
 
 
-## x is a table with:
-## - a column called 'dbh' which has dbh of every stems in mm
-## - a column called 'SPPCODE' with species 4-letter species code 
+## Organize your data in this case, 
+##  x is a table with:
+# a column called 'dbh' which has dbh of every stems in mm
+# a column called 'SPPCODE' which has species 4-letter species code (this sp code is unique per site)
 
 x$agb_ctfs  <-  x$agb
 
@@ -18,10 +19,10 @@ list.object.before <- ls()
 #Calculating biomass in shrubs is a little complicated, so we came up with 2 approaches:
 
 # list of shrubs species ####
-
-shrub.DBH.sp <- c("beth", "chvi", "coam", "crpr", "elum", "eual", "ilve", "libe", "saca") # using DBH in AGB allometric equation (approach 1)
-
-shrub.BD.sp <- c("havi", "loma", "romu","rual", "rupe", "ruph", "viac", "vipr", "vire") # using basal diameter in AGB allometric equation (approach 2)
+# Approach 1
+shrub.DBH.sp <- c("beth", "chvi", "coam", "crpr", "elum", "eual", "ilve", "libe", "saca") # using DBH in AGB allometric equation
+# Approach 2
+shrub.BD.sp <- c("havi", "loma", "romu","rual", "rupe", "ruph", "viac", "vipr", "vire") # using basal diameter in AGB allometric equation
 
 
 # Case 1: Calculate Biomass for shrubs using a DBH allometry equation ####
@@ -64,7 +65,6 @@ tree.main.stem.DBH$agb  <- ifelse(tree.main.stem.DBH$sp == "libe", exp(-2.2118 +
 
 tree.main.stem.DBH$agb  <- ifelse(tree.main.stem.DBH$sp == "saca", exp(-2.48 + 2.4835 * log(tree.main.stem.DBH$dbh * 0.1)) * 0.36, tree.main.stem.DBH$agb )
 
-
 ## Redistribute the biomass of main stem to other stems, using the basal contribution
 tree.main.stem.AGB <- tree.main.stem.DBH$agb[match(x.shrub.DBH$tag, rownames(tree.main.stem.DBH))] #get a vector as long as x.shrub.DBH with AGB of main stem repeated for each stem of tree
 
@@ -83,13 +83,13 @@ tree.sum.BA.un <- tapply(BA, x.shrub.BD$tag, sum, na.rm = T) #sum of basal areas
 tree.sum.BA.rep <- tree.sum.BA.un[match(x.shrub.BD$tag, names(tree.sum.BA.un))] # same but same length as BA
 BA.contribution <- BA / tree.sum.BA.rep #contribution of each stem to sum of basal area of tree
 
-
-## calulate diameter at base of shrub, assuming area preserving
+## calculate diameter at base of shrub, assuming area preserving
 tree.BD <- data.frame(tag = names(tree.sum.BA.un), BD = sqrt(tree.sum.BA.un/pi) * 2)
 tree.BD <- merge(tree.BD, x.shrub.BD[, c("tag", "sp")], by = "tag")
 
 #multiple dbh in mm by 0.1 to obtain dbh in cm
-#divide by 1000 to obtain biomass in kg (as original pub give biomass in gr) 
+#divide by 1000 to obtain biomass in kg (as original pub gives biomass in gr) 
+
 ## calculate AGB using basal diamter
 
 tree.BD$agb <- NA
@@ -120,7 +120,11 @@ x.shrub.BD$agb = tree.BD$agb * BA.contribution
 
 
 
-# Now calculate AGB for tree species ####
+# Now calculate AGB for tree species, take into consideration the following:
+# multiple dbh in mm by 0.1 to obtain dbh in cm
+#some species will need to multiply by the bias correction factor reported in the original publication (ie. 0.36; 1.008)
+#multiply by 0.03937 if original pub gived dbh in inch
+#multiply by 0.45359 if original pub give AGB in lbs
 
 x.trees <- x[!x$sp %in% c(shrub.DBH.sp, shrub.BD.sp),]
 
@@ -175,7 +179,7 @@ x.trees$agb <- ifelse(x.trees$sp == "elum", exp(-2.48 + 2.4835 * log(x.trees$dbh
 
 x.trees$agb <- ifelse(x.trees$sp == "eual", exp(-2.48 + 2.4835 * log(x.trees$dbh * 0.1)), x.trees$agb)
 
-x.trees$agb <- ifelse(x.trees$sp == "fagr", (2.0394 * (x.trees$dbh * 0.03937)^2.5715) * 0.45359, x.trees$agb)
+x.trees$agb <- ifelse(x.trees$sp == "fagr", 10^(2.1112 + 2.462 * log10(x.trees$dbh * 0.1)) / 1000, x.trees$agb)# new equation added by Erika 1/13/2020
 
 x.trees$agb <- ifelse(x.trees$sp == "fram", (2.3626 * (x.trees$dbh * 0.03937)^2.4798) * 0.45359, x.trees$agb)
 
@@ -214,15 +218,14 @@ x.trees$agb <- ifelse(x.trees$sp == "ploc" & (x.trees$dbh * 0.1) >= 24, (2.51502
 
 x.trees$agb <- ifelse(x.trees$sp == "prav", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)
 
-x.trees$agb <- ifelse(x.trees$sp == "prpe", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)
-
-x.trees$agb <- ifelse(x.trees$sp == "prse", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)
+#x.trees$agb <- ifelse(x.trees$sp == "prse", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)
+x.trees$agb <- ifelse(x.trees$sp == "prse", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)                                            
 
 x.trees$agb <- ifelse(x.trees$sp == "prsp", (1.8082 * (x.trees$dbh * 0.03937)^2.6174) * 0.45359, x.trees$agb)
 
 x.trees$agb <- ifelse(x.trees$sp == "qual", (1.5647 * (x.trees$dbh * 0.03937)^2.6887) * 0.45359, x.trees$agb)
 
-x.trees$agb <- ifelse(x.trees$sp == "quco", (2.6574 * (x.trees$dbh * 0.03937)^2.4395) * 0.45359, x.trees$agb)
+x.trees$agb <- ifelse(x.trees$sp == "quco", 10^((1.283 + 2.685 * log10(x.trees$dbh * 0.1)) / 1000) *1.003, x.trees$agb)# new equation added by Erika 1/13/2020
 
 x.trees$agb <- ifelse(x.trees$sp == "qufa" & x.trees$dbh <= 26 * 25.4, (2.3025 * ((x.trees$dbh * 0.03937)^2)^1.2580) * 0.45359, x.trees$agb)
 x.trees$agb <- ifelse(x.trees$sp == "qufa" & x.trees$dbh > 26 * 25.4,  (2.2373 * ((x.trees$dbh * 0.03937)^2)^1.2639) * 0.45359, x.trees$agb)
@@ -245,7 +248,7 @@ x.trees$agb <- ifelse(x.trees$sp == "quve" & (x.trees$dbh * 0.1) >= 30, (10^(1.0
 x.trees$agb <- ifelse(x.trees$sp == "rops", exp(-2.5095 + 2.5437 * log(x.trees$dbh * 0.1)), x.trees$agb)
 
 # x.trees$agb <- ifelse(x.trees$sp == "saal", (10^(1.3539  + 1.3412 * log10(x.trees$dbh * 0.1)^2)) / 1000 * 1.004, x.trees$agb)
-x.trees$agb <- ifelse(x.trees$sp == "saal", exp(-2.2118 + 2.4133 * log(x.trees$dbh * 0.1)) , x.trees$agb) # new equation given by Erika on Tue 4/2/2019 11:57
+x.trees$agb <- ifelse(x.trees$sp == "saal", exp(-2.2118 + 2.4133 * log(x.trees$dbh * 0.1)) , x.trees$agb) # new equation given by Erika on Tue 4/2/2019 11:57 (Chojnacky)
 
 
 # x.trees$agb <- ifelse(x.trees$sp == "tiam", (1.4416 * (x.trees$dbh * 0.03937)^2.7324) * 0.45359, x.trees$agb)
@@ -270,7 +273,7 @@ x.all <- rbind(x.trees, x.shrub.DBH, x.shrub.BD)
 x.all <- x.all[order(x.all$order),]
 x.all$order <- NULL
 
-if(!identical(x.all$dbh, x$dbh)) stop("order is not he same as the begining")
+if(!identical(x.all$dbh, x$dbh)) stop("order is not the same as the begining")
 x <- x.all
 
 #Convert from kg to Mg
